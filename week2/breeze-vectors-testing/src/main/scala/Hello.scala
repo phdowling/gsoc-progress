f1dfd4db8c5abf8dd1386b2df1a1e67fcc2e397a
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-06-29 15:39:26 +0200

    fix vector testing code

--- week2/breeze-vectors-testing/src/main/scala/Hello.scala
+++ week2/breeze-vectors-testing/src/main/scala/Hello.scala
@@ -11,12 +11,11 @@ object Hello{
   def main (args: Array[String]) {
     println("Hello World!")
     val data_dir = args(0)
-    val modelpath = data_dir + "/word2vec_test.csv"
-    val dictpath = data_dir + "/word2vec_test_wordids.txt"
+    val modelpath = data_dir + ".syn0.csv"
+    val dictpath = data_dir + ".wordids.txt"
     val w2v = new Word2VecWrapper(modelpath, dictpath)
     println(w2v.get_similarity("affiliated", "advancement"))
     println(w2v.get_similarity("affiliated people".split(" "), "advancement houses".split(" ")))
-    println(w2v.get_similarity("affiliated people asfgaw".split(" "), "advancement adadewef houses".split(" ")))
 
   }
 
