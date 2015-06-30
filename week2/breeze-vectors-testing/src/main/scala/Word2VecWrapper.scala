c08b2e724f07225065099f6d924632091a8e41c0
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-06-30 21:11:58 +0200

    fix cosine sim

--- week2/breeze-vectors-testing/src/main/scala/Word2VecWrapper.scala
+++ week2/breeze-vectors-testing/src/main/scala/Word2VecWrapper.scala
@@ -10,6 +10,13 @@ import scala.io.Source
 /**
  * Created by dowling on 04/06/15.
  */
+
+def cosine_similarity(vector1: Transpose[DenseVector[Double]], vector2: Transpose[DenseVector[Double]]): Double = {
+  (vector1 * vector2.t) /
+    (sqrt(vector1 * vector1.t) * sqrt(vector2 * vector2.t))
+}
+
+
 class Word2VecWrapper(modelPath: String, dictPath: String) {
   println("Read dict..")
   var dict: Map[String, Int] = Source.fromFile(dictPath, "UTF-8").getLines().map { line =>
@@ -18,27 +25,25 @@ class Word2VecWrapper(modelPath: String, dictPath: String) {
   }.toMap
 
   println("Read weights..")
-  val matrixSource = io.Source.fromFile(modelPath)
-  val lines = matrixSource.getLines()
-  val rows = lines.next().substring(2).toInt
-  val cols = lines.next().substring(2).toInt
-  var vectors: DenseMatrix[Double] = DenseMatrix.zeros[Double](rows, cols)
-  lines.zipWithIndex.foreach{case (row_str, row_idx) =>
-    if(row_idx % 10000 == 0)
-      println("At row " + row_idx)
-    val vals = row_str.split(",").map(_.trim).map(_.toDouble)
-    vals.zipWithIndex.foreach{case (value, col_idx) => vectors(row_idx, col_idx) = value}
-  }
-  matrixSource.close()
+  val vectors: DenseMatrix[Double] = read_weights_csv
 
-  //val reader = CSVReader.open(new File("sample.csv")).toStream()
-  //val head_str = reader.head()
-  //var vectors: DenseMatrix[Double] = DenseMatrix.zeros[Double](4000000, 100)
-  //reader.zipWithIndex.foreach{ case (row, row_idx) =>
-  //  row.map(_.toDouble).zipWithIndex.foreach{ case (value, col_idx) =>
-  //
-  //  }
-  //}
+  def read_weights_csv: DenseMatrix[Double] = {
+    val matrixSource = io.Source.fromFile(modelPath)
+    val lines = matrixSource.getLines()
+    val rows = lines.next().substring(2).toInt
+    val cols = lines.next().substring(2).toInt
+    println("Allocating " + rows + "x" + cols + " matrix..")
+    val vectors: DenseMatrix[Double] = DenseMatrix.zeros[Double](rows, cols)
+    println("Reading CSV and writing to matrix...")
+    lines.zipWithIndex.foreach { case (row_str, row_idx) =>
+      if (row_idx % 10000 == 0)
+        println("At row " + row_idx)
+      val values = row_str.split(",").map(_.trim).map(_.toDouble)
+      values.zipWithIndex.foreach { case (value, col_idx) => vectors(row_idx, col_idx) = value }
+    }
+    matrixSource.close()
+    vectors
+  }
 
 
   def lookup(token: String): Transpose[DenseVector[Double]]={
@@ -47,6 +52,7 @@ class Word2VecWrapper(modelPath: String, dictPath: String) {
     if(dict.contains(token)){
       vectors(dict(token), ::)
     }else{
+      println("Warning: token " + token + " not in dictionary! Lookup returning null vector.")
       DenseVector.zeros[Double](vectors.cols).t
     }
   }
@@ -54,14 +60,14 @@ class Word2VecWrapper(modelPath: String, dictPath: String) {
   def get_similarity(first: String, second:String): Double = {
     // todo: do we need 1 - (lookup(first) * lookup(second).t) ?
 
-    lookup(first) * lookup(second).t
+    cosine_similarity(lookup(first), lookup(second))
   }
 
   def get_similarity(first: Array[String], second: Array[String]): Double = {
     val f = first.map(lookup).reduceLeft(_ + _)
     val s = second.map(lookup).reduceLeft(_ + _)
 
-    f * s.t
+    cosine_similarity(f, s)
   }
 
 }
